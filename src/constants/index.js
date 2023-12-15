5f3a40a83660cea7c1610b48417f6712d4fd1137
Arunav Raghav <arunavraghav2003@gmail.com>
2023-12-16 00:39:29 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,232 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Web Developer Intern",
+        company_name: "Coding Raja Technologies",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "June 2023 - July 2023",
+        points: [
+            "Developing and Creating various Projects Using Web Technologies Like React.js,BootStrap and JavaScript.",
+            
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+            "Created Various Projects Like 3D Landing Pages.",
+        ],
+    },
+    {
+        title: "Web Developer Intern",
+        company_name: "Coding Raja Technologies",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "June 2023 - July 2023",
+        points: [
+            "Developing and Creating various Projects Using Web Technologies Like React.js,BootStrap and JavaScript.",
+            
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+            "Created Various Projects Like 3D Landing Pages.",
+        ],
+    },{
+        title: "Web Developer Intern",
+        company_name: "Coding Raja Technologies",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "June 2023 - July 2023",
+        points: [
+            "Developing and Creating various Projects Using Web Technologies Like React.js,BootStrap and JavaScript.",
+            
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+            "Created Various Projects Like 3D Landing Pages.",
+        ],
+    },
+    {
+        title: "Web Developer Intern",
+        company_name: "Coding Raja Technologies",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "June 2023 - July 2023",
+        points: [
+            "Developing and Creating various Projects Using Web Technologies Like React.js,BootStrap and JavaScript.",
+            
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+            "Created Various Projects Like 3D Landing Pages.",
+        ],
+    },
+
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/ArunavRaghav',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/ArunavRaghav',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Apple Vision 3D clone',
+        description: 'Developed Apple Vision Clone Website with the help of GSAP, Scroll Trigger,Locomotive Js.',
+        link: 'https://apple-vision-clone-alpha.vercel.app/',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: '3d Landing Page',
+        description: 'Created a 3d landing page that consist of a character which keeps on moving while scrolling .',
+        link: 'https://3d-website-psi.vercel.app/',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Realtime Weather App',
+        description: 'Created a realtime weather app with the help of Html,Css and basic JavaScript with API Call',
+        link: ' https://arunav-raghav-weatherapp.vercel.app/',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Calculater With Toggle Mode',
+        description: 'Built a Simple calculator with toggling mode using various frontend technologies.',
+        link: 'https://arunavraghav-calculator.vercel.app/',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Drum-Kit Player',
+        description: 'Developed a website Based Drum Sound Player with the help of various frontend technologies and basic JavaScript that produces sound on key press.',
+        link: 'https://drum-player-arunavraghav.vercel.app/',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Basic Portfolio',
+        description: 'Created a Simple Portfolio Using Html And Css Only which is responsive and adjustable according to any screen size.',
+        link: 'https://arunavraghav.vercel.app/',
+    }
+];
